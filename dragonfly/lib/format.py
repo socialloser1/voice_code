b00fd104ff9dc0ac9e715b8fc70e11ea3798537a
Simon Larsén <slarse@kth.se>
2016-05-26 11:18:34 +0200

    Add camel case to format.py #3

--- dragonfly/lib/format.py
+++ dragonfly/lib/format.py
@@ -14,9 +14,27 @@ with snake case formatting.
 '''
 def snake_case( text ):
     new_text = ""
-    words = str(text).split()
+    words = str( text ).split()
     for word in words:
         if new_text != "" and new_text[-1:].isalnum() and word[-1:].isalnum():
             word = "_" + word
         new_text += word.lower()
     return new_text
+
+''' Takes a string input and returns the same string
+with camel case formatting
+'''
+def camel_case( text ):
+    newText = ""
+    text = str( text ).lower()
+    capNext = False
+
+    for char in text:
+        if char == " " :
+            capNext = True
+        elif capNext:
+            newText += char.upper()
+            capNext = False
+        else:
+            newText += char
+    return newText
