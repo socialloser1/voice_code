469275f24213f22f96e7d16dd0d2cc01bad9be41
Simon Larsén <slarse@kth.se>
2016-05-26 11:00:49 +0200

    fixed indentation issues

--- dragonfly/lib/format.py
+++ dragonfly/lib/format.py
@@ -2,7 +2,7 @@
 
 This module contains general formatting functions.
 
-Author: Simon Larsén
+Author: Simon Larsen
 Version: 2016-05-26
 
 '''
@@ -14,9 +14,9 @@ with snake case formatting.
 '''
 def snake_case( text ):
     new_text = ""
-      words = str(text).split()
-      for word in words:
-          if new_text != "" and new_text[-1:].isalnum() and word[-1:].isalnum():
-              word = "_" + word
-          new_text += word.lower()
+    words = str(text).split()
+    for word in words:
+        if new_text != "" and new_text[-1:].isalnum() and word[-1:].isalnum():
+            word = "_" + word
+        new_text += word.lower()
     return new_text
