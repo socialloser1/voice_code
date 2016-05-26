cd0f5731f47e22c24624476b1ce5f1703b2143aa
Simon Larsén <slarse@kth.se>
2016-05-26 10:47:35 +0200

    Add dragonfly scripts folder; Add general formatting module w. snake case #3

---
+++ dragonfly/lib/format.py
@@ -0,0 +1,22 @@
+'''Module format
+
+This module contains general formatting functions.
+
+Author: Simon Larsén
+Version: 2016-05-26
+
+'''
+
+
+''' Takes a string input and returns the same string
+with snake case formatting.
+
+'''
+def snake_case( text ):
+    new_text = ""
+      words = str(text).split()
+      for word in words:
+          if new_text != "" and new_text[-1:].isalnum() and word[-1:].isalnum():
+              word = "_" + word
+          new_text += word.lower()
+    return new_text
