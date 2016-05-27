880c803ade7dce407ec2fdd97bbd7af017b5bab5
Simon Larsén <slarse@kth.se>
2016-05-27 10:56:20 +0200

    Update _global.py. this closes #5

--- dragonfly/lib/_global.py
+++ dragonfly/lib/_global.py
@@ -11,17 +11,22 @@ from dragonfly import ( Key, Function, Grammar,
 
 import format
 
+# Functions that execute different kinds of formatting on text
 def snake_case_format( text ):
 	Text( format.snake_case( text ) ).execute()
 
 def camel_case_format( text ):
 	Text( format.camel_case( text ) ).execute()
 
+def pascal_case_format( text ):
+	Text( format.pascal_case( text ) ).execute()
+
 class MainRule( MappingRule ):
 
 	mapping = { 
         "[use] snay cass [<text>]" : Function( snake_case_format, extra = {"text"} ),
         "[use] cam cass [<text>]" : Function( camel_case_format, extra = {"text"} ),
+        "[use] pass cass [<text>]": Function( pascal_case_format, extra = {"text"} ),
 	}
 	extras = [
                 Dictation( "text" ),
