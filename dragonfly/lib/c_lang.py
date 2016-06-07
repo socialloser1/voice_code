7df25f0cc25aa3d965d5a9a025fc1e439a408d45
Simon Larsén <slarse@kth.se>
2016-06-07 16:33:06 +0200

    Add while and for loops, close #30

--- dragonfly/lib/c_lang.py
+++ dragonfly/lib/c_lang.py
@@ -8,6 +8,7 @@ Version: 2016-05-31
 """
 
 import format
+from general_programming import SymbolsRule
 
 from dragonfly import (
     Function,
@@ -29,18 +30,33 @@ class KeywordsRule(MappingRule):
         "standard bool": "stdbool.h",
     }
 
+    boolean_ints = {
+        "True": 1,
+        "False": 0,
+    }
+
     mapping = {
-        "if": Text("if () {") + Key("enter:2") + Text("}") + Key("up:2, end, left:3"),
-        "else if": Text("else if () {") + Key("enter:2") + Text("}")
-        + Key("up:2, end, left:3"),
+        "if": Text("if () {") + Key("enter") + Text("}") + Key("up, end, left:3"),
+        "else if": Text("else if () {") + Key("enter") + Text("}")
+        + Key("up, end, left:3"),
         "else": Text("else {}") + Key("left, enter, up, end, enter"),
+        "while": Text("while () {") + Key("enter, rbrace, up, end, left:3"),
+        "for": Text("for ()  {") + Key("enter, rbrace, up, end, left:3"),
+        "for range <i> increment": Text("for (int i = 0, i < %(i)s, i++) {}")
+        + Key("left, enter, up, end, enter"),
+        "for range <i> decrement": Text("for (int i = %(i)s, 0 <= i, i--) {}")
+        + Key("left, enter, up, end, enter"),
         "include": Text("#include <>") + Key("left"),
         "include <package>": Text("#include <%(package)s>"),
         "print": Text("printf();") + Key("left:2"),
-        "print string": Text('printf("");') + Key("left:3"),
+        "print string": Text('printf("\\n");') + Key("left:5"),
+        "return": Text("return ;") + Key("left"),
+        "return <boolean_int>": Text("return %(boolean_int)d;"),
     }
     extras = [
         Choice("package", common_packages),        
+        Choice("boolean_int", boolean_ints),
+        Integer("i", 0, 10000),
     ]
 
 """ Below is is everythong needed to declare functions, classes and variables """
@@ -71,7 +87,7 @@ def declare_unsigned(data_type, text):
 def define_function(data_type, text):
     Text("%s %s() {"
             %(data_type, format.snake_case(text))).execute()
-    Key("enter:2, rbrace, up:2, end, left:3").execute()
+    Key("enter, rbrace, up, end, left:3").execute()
 
 class VariablesRule(MappingRule):
 
@@ -103,6 +119,7 @@ class VariablesRule(MappingRule):
 grammar = Grammar("c")
 grammar.add_rule(VariablesRule())
 grammar.add_rule(KeywordsRule())
+grammar.add_rule(SymbolsRule())
 grammar.load()
 grammar.disable()
 
