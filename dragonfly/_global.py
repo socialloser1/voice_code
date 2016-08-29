5a2037407744ba4f477e09c324592d811f09b721
Simon Larsen <slarse@kth.se>
2016-08-29 11:09:18 +0200

    Re-push entire file

--- dragonfly/_global.py
+++ dragonfly/_global.py
@@ -9,7 +9,7 @@ Version: 2016-05-26
 from dragonfly import ( Key, Function, Grammar,
                         Dictation, MappingRule, Text, Choice, Integer )
 
-from lib import (total_war, general_programming, format, python, c_lang, vim)
+from lib import (java, general_programming, format, python, c_lang, vim)
 
 # Functions that execute different kinds of formatting on text
 def snake_case_format(text):
@@ -64,6 +64,7 @@ class MainRule( MappingRule ):
         "vim": vim,
         "general programming": general_programming,
         "see": c_lang,
+        "java": java,
     }
 
     def enabled_modules():
