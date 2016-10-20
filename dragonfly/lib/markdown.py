7f0f0fda715d7899f04b6a0f8fe60126f7c491dc
Simon Larsen <slarse@kth.se>
2016-10-20 19:49:24 +0200

    Add skeleton for markdown module

---
+++ dragonfly/lib/markdown.py
@@ -0,0 +1,24 @@
+"""
+This module contains voice commands for formatting Markdown.
+
+Author: Simon Larsen
+Version: 2016-10-20
+"""
+
+from dragonfly import ( Key, Function, Grammer, Dictation, MappingRule,
+                        Text, Choice, Integer )
+
+from lib import(format)
+
+class FormatRule(MappingRule):
+    """ This is the main rule for the Markdown module.
+    It contains all of the formatting commands. """
+
+    mapping = [
+    ]
+    extras = [
+    ]
+
+grammar = Grammar("markdown")
+grammar.add_rule(FormatRule())
+grammar.load()
